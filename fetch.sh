0b94b2d0c2204a33454293a3e262343679842775
Aldrin Morris D. Balitbit <aldrinbalitbit1127@gmail.com>
2024-10-31 20:38:44 +0800

    See the error

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-FREEBSD_VERSION="$(curl -s https://download.freebsd.org/snapshots/amd64/ | grep -oP '(?<=href=")[^"/]+(?=/")' | grep -E '^1[4-9]\.[0-9]+-CURRENT' | sort -V | tail -1)"
+FREEBSD_VERSION="$(curl -s https://download.freebsd.org/snapshots/amd64/ | ggrep -oP '(?<=href=")[^"/]+(?=/")' | grep -E '^1[4-9]\.[0-9]+-CURRENT' | sort -V | tail -1)"
 for i in base lib32; do
     echo "downloading https://download.freebsd.org/snapshots/amd64/${FREEBSD_VERSION}/${i}.txz"
     wget -q "https://download.freebsd.org/snapshots/amd64/${FREEBSD_VERSION}/${i}.txz"
